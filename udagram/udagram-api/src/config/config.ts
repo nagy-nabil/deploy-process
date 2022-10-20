b7e1146d724a9fa6d0a2f8a113c93b2798e225d2
nagy nabil <nagynabil651@gmail.com>
2022-10-20 12:54:00 +0200

    eb env vars from circleci

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -7,13 +7,13 @@ dotenv.config();
 // Are Also needed
 
 export const config = {
-  username: `${process.env.POSTGRES_USERNAME}`,
+  username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
-  aws_region: process.env.AWS_REGION,
-  aws_profile: process.env.AWS_PROFILE,
-  aws_media_bucket: process.env.AWS_BUCKET,
+  awsRegion: process.env.AWS_REGION,
+  awsProfile: process.env.AWS_PROFILE,
+  awsBucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
     secret: process.env.JWT_SECRET,
